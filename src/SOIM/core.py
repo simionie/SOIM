1b1e802d56426578bd45c579688a9b70e1627843
Romolo Politi <romolo.politi@inaf.it>
2024-01-17 13:43:32 +0100

    manual pool

--- src/SOIM/core.py
+++ src/SOIM/core.py
@@ -91,7 +91,8 @@ def core_soim(project_list: dict, latest, kernel_folder,output_folder,suppress=F
         args=[(k, v, latest, kernel_folder, output_folder, suppress)
          for k, v in project_list.items()]
         while True:
-            console.log(f"{len(proc_array)=}, {current_proc=}")
+            console.log(f"{len(proc_array)=}, {current_proc=}, {len(project_list)=}, Evaluete= {len(
+                proc_array) == 0 or (len(proc_array) < num_processes and current_proc != len(project_list))}")
             # console.log(current_proc)
             if len(proc_array)==0 or (len(proc_array)<num_processes and current_proc != len(project_list)):
                 console.print(args[current_proc])
